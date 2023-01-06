a8ce329f69311f0e561d2f3400647808dd8b67bb
xramas <mastercvmo@gmail.com>
2023-01-06 19:59:11 +0800

    Update

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -35,8 +35,8 @@ class Footer extends Component {
                         </a>
                         <p class="is-size-7">
                             <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
-                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">Hexo</a>&nbsp;&&nbsp;
-                            <a href="https://github.com/ppoffice/hexo-theme-icarus" target="_blank" rel="noopener">Icarus</a>
+                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">HEXO</a>&nbsp;&&nbsp;
+                            <a href="https://github.com/xramas/hexo-theme-icarus" target="_blank" rel="noopener">ICARUS</a>
                             {showVisitorCounter ? <br /> : null}
                             {showVisitorCounter ? <span id="busuanzi_container_site_uv"
                                 dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span> : null}
